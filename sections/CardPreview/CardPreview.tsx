0e70a72314cc8df4877ad60803ae86cc7cd9210f
junielson_tardin_corebiz <junielson_tardin_corebiz@whirlpool.com>
2024-07-14 11:54:28 -0300

    removing useless hx attribute

--- sections/CardPreview/CardPreview.tsx
+++ sections/CardPreview/CardPreview.tsx
@@ -12,7 +12,7 @@ export default function CardPreview({
       id="card-preview"
     >
       <div class="space-y-16">
-        <div class="flex flex-col lg:flex-row gap-4 justify-between" hx-preserve>
+        <div class="flex flex-col lg:flex-row gap-4 justify-between">
           <div class="space-y-6 lg:w-1/2">
             <h2 class="text-[#02F67C] text-4xl fw7">
               {title}
